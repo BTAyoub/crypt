5cbdc66dd5aab397d9f6b9dadb0f724badcb1e09
ayoub <153848916+BTAyoub@users.noreply.github.com>
2025-07-23 20:57:12 +0100

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-app = Flask("")
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Bot is running..."
+    return "Bot is alive!"
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-if __name__ == "__main__":
-    run()
+def keep_alive():
+    app.run(host='0.0.0.0', port=8080)
